4f518a55802de1b8705dfd374c43f08e6dd3c65a
Can Taşkın <94763376+cantaskin@users.noreply.github.com>
2024-05-03 06:12:32 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarManager carManager = new CarManager(new InMemoryCar());
+
+        foreach (var car in carManager.GetAll())
+        {
+            Console.WriteLine(car.BrandId + car.ModelYear + car.ColorId);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryBrand.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryBrand : IBrandDal
+    {
+        List<Brand> _brands;
+
+        public InMemoryBrand()
+        {
+            _brands = new List<Brand>
+                {
+                    new Brand { BrandId = 0, BrandName = "BMW" },
+                    new Brand { BrandId = 1, BrandName = "Mercedes" }
+                };
+        }
+        public void Add(Brand brand)
+        {
+            _brands.Add(brand);
+        }
+
+        public void Delete(Brand brand)
+        {
+            Brand brandToDelete = null;
+            brandToDelete = _brands.SingleOrDefault(p => p.BrandId == brand.BrandId);
+            if (brandToDelete != null)
+                _brands.Remove(brandToDelete);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brands;
+        }
+
+        public Brand GetById(int id)
+        {
+            return _brands.SingleOrDefault(b => b.BrandId == id);
+        }
+
+        public void Update(Brand brand)
+        {
+            Brand brandToUpdate = null;
+            brandToUpdate = _brands.SingleOrDefault(p => p.BrandId == brand.BrandId);
+            if (brandToUpdate != null)
+                brandToUpdate = brand;
+        }
+    }
+}
